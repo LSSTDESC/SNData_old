69fe0cdd66d6f5aeaf76591e3c79ea6a81b9fd36
R. Biswas <rbiswas4@gmail.com>
2017-03-09 21:07:46 -0800

    Rebuilding docs to work with the readthedocs theme and jekyll
    - Adding files for documentation
    - Added file contents for these files
    - Changed to readthedocs theme
    - Adding files that have been missed in docs
    - Checked that .nojekyll is not what we want, as it will not allow
      index.md files then
    - Adding _config.yml file to include directories starting with underscores in
    jekyll
    - help from github pages staff!
    
    	new file:   docs/build/html/API.html
    	new file:   docs/build/html/genindex.html
    	new file:   docs/build/html/index.html
    	new file:   docs/build/html/overview.html
    	new file:   docs/build/html/py-modindex.html
    	new file:   docs/build/html/requirements.html
    	new file:   docs/build/html/search.html
    	new file:   docs/_config.yml

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -94,24 +94,32 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = 'sphinx_rtd_theme'
-html_theme = 'alabaster'
-#html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme = 'sphinx_rtd_theme'
+#html_theme = 'alabaster'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-#html_theme_options = {
-#                     'collapse_navigation': False,
-#                     'display_version': False,
-#                     }
+html_theme_options = {
+                     'collapse_navigation': False,
+                     'display_version': False,
+                     }
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['static']
 
+html_sidebars = {
+    '**': [
+        'about.html',
+        'navigation.html',
+        'relations.html',
+        'searchbox.html',
+    ]
+}
 
 # -- Options for HTMLHelp output ------------------------------------------
 
