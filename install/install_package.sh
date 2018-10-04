8f9834ef3d6951408c59b8fb22231f1285da7502
R. Biswas <rbiswas4@gmail.com>
2018-10-04 22:41:23 +0200

    Echo 	modified:   install_package.sh

--- install/install_package.sh
+++ install/install_package.sh
@@ -3,6 +3,7 @@ pushd ../
 git clone  https://github.com/lsst/throughputs
 cd throughputs
 export THROUGHPUTS_DIR=`pwd`
+echo "THROUGHPUTS set to " $THROUGHPUTS_DIR
 popd 
 python setup/generate_requirements.py
 python setup.py install --user
