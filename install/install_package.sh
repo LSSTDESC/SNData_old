ae8da2cf1f0dc6dde35f30256a335a19a76b59a4
R. Biswas <rbiswas4@gmail.com>
2018-10-04 22:07:19 +0200

    Added throughputs install 	modified:   install_package.sh

--- install/install_package.sh
+++ install/install_package.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
+pushd ../
+git clone  https://github.com/lsst/throughputs
+cd throughputs
+export THROUGHPUTS_DIR=`pwd`
+popd 
 python setup/generate_requirements.py
 python setup.py install --user
