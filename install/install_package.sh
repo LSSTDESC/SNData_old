06c46c956226950ad46bee723a70b0eaa8b44049
R. Biswas <rbiswas4@gmail.com>
2018-10-04 22:57:39 +0200

    Adding to before_script in travis.yml 	modified:   .travis.yml 	modified:   install/install_package.sh 	new file:   setup/add_throughputs.sh

--- install/install_package.sh
+++ install/install_package.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
-pushd ../
-git clone  https://github.com/lsst/throughputs
-cd throughputs
-export THROUGHPUTS_DIR=`pwd`
-echo "THROUGHPUTS set to " $THROUGHPUTS_DIR
-popd 
 python setup/generate_requirements.py
 python setup.py install --user
---
+++ setup/add_throughputs.sh
@@ -0,0 +1,3 @@
+pushd ../
+git clone  https://github.com/lsst/throughputs
+popd 
